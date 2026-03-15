afd44f33291326a0d6e26549ab8f9c2678b84b3c
poojaabhang28-cyber <poojaabhang28@gmail.com>
2026-03-16 03:49:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -402,5 +402,8 @@ def about():
 
 # ---------------- RUN SERVER ----------------
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
